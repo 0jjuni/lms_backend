722ef609508a77cef051335065fed1f689ba10b7
0jjuni <133225625+0jjuni@users.noreply.github.com>
2024-05-30 21:05:53 +0900

    [subject] enrollments 엔드포인트에서 get요청을 보낼 때, subject 모델의 모든 정보를 추가하여 출력

--- subject/serializers.py
+++ subject/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Enrollment
+from .models import Enrollment, Subject
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ['subject_code', 'subject_name', 'subject_div', 'professor', 'classroom']
+
+
 
 class EnrollmentSerializer(serializers.ModelSerializer):
+    subject = SubjectSerializer()
     class Meta:
         model = Enrollment
         fields = '__all__'
