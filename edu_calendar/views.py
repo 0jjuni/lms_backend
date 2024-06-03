0c4a796fb6f14bda848208bdc7f1cc2165086c0f
0jjuni <133225625+0jjuni@users.noreply.github.com>
2024-06-03 22:57:59 +0900

    [hotfix] calendar의 update가 안되는 오류 수정

--- edu_calendar/views.py
+++ edu_calendar/views.py
@@ -23,8 +23,17 @@ class PersonalCalendarViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(enrollment_number=self.request.user)
 
-    def perform_update(self, serializer, instance):
-        serializer.save(enrollment_number=self.request.user)
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        serializer.save(enrollment_number=self.request.user, instance=instance)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
     def get_queryset(self):
         user = self.request.user
