06264bcac2b196756f4684326720fc77f533d342
g2ro <kilho603@gmail.com>
2024-06-04 15:15:01 +0900

    [syllabus] 시리얼라이즈 구현_ 수정필요

--- syllabus/models.py
+++ syllabus/models.py
@@ -5,7 +5,7 @@ from subject.models import Subject
 class Syllabus(models.Model):
     subject_code = models.ForeignKey(Subject, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    context = models.TextField()
+    text = models.TextField()
 
     def __str__(self):
         return self.title
--- syllabus/serializers.py
+++ syllabus/serializers.py
@@ -1,20 +1,36 @@
 from rest_framework import serializers
 from .models import Syllabus
 from subject.models import Subject
-class SyllabusSerialziers(serializers.Serializer):
+#강의자료 생성
+class SyllabusSerialziers(serializers.ModelSerializer):
+    subject_code = serializers.SlugRelatedField(
+        slug_field='subject_code',  # Subject 모델에서 subject_code 필드를 찾기 위한 키
+        queryset=Subject.objects.all(),
+        error_messages={
+            'does_not_exist': '입력한 subject_code(과목코드) {value}는 유효하지 않습니다.'
+        }
+    )
+
     class Meta:
         model = Syllabus
         fields = '__all__'
-        read_only_fields = ['subject_code', ]
+
+    def __init__(self, *args, **kwargs):
+        super(SyllabusSerialziers, self).__init__(*args, **kwargs)
+        # 수정 요청인 경우 subject_code 필드를 읽기 전용으로 설정
+        if self.context['request'].method in ['PUT', 'PATCH']:
+            self.fields['subject_code'].read_only = True
 
     def create(self, validated_data):
         request = self.context['request']
         user = request.user
 
-        subject_code = validated_data.get('subject_code')
-        if not subject_code:
-            raise serializers.ValidationError("Body에 subject_code(과목코드)가 없습니다.")
+        if user and hasattr(Syllabus, 'author'):
+            validated_data['author'] = user
 
-        # Subject 테이블에서 subject_code 확인
-        if not Subject.objects.filter(subject_code=subject_code).exists():
-            raise serializers.ValidationError("해당 subject_code(과목코드)가 Subject 테이블에 존재하지 않습니다.")
+        return super().create(validated_data)
+
+    def validate(self, data):
+        if 'subject_code' not in data:
+            raise serializers.ValidationError({"subject_code": "Body에 subject_code(과목코드)가 없습니다."})
+        return data
